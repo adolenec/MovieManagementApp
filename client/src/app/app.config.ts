e3afd8909c0a3c640b5416300da4774b0c3edc1b
adolenec <andrija.dolenec23@gmail.com>
2023-09-04 08:14:11 +0200

    Initial commit

---
+++ client/src/app/app.config.ts
@@ -0,0 +1,15 @@
+import { ApplicationConfig, InjectionToken } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { APP_ROUTES } from './app.routes';
+
+import { provideHttpClient } from '@angular/common/http';
+
+export const API_URL = new InjectionToken<string>('apiUrl');
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    { provide: API_URL, useValue: 'https://localhost:4000' },
+    provideRouter(APP_ROUTES),
+    provideHttpClient(),
+  ],
+};
